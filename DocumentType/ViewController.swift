d8608f76d14509707b88b0abad8b3766b93224d9
Wink <a318647551@163.com>
2021-01-23 17:13:38 +0800

    Initial Commit

---
+++ DocumentType/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DocumentType
+//
+//  Created by Wink on 2021/1/23.
+//  Copyright © 2021 Wink. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
