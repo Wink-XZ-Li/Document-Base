2f03515babd7f1b0238225e1cda99b6e718dbe04
李子恂 <wink@MacBook-Pro.mshome.net>
2021-01-23 22:01:34 +0800

    merge files

---
+++ DocumentType/DocumentViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DocumentViewController.swift
+//  DocumentType
+//
+//  Created by Wink on 2021/1/23.
+//  Copyright © 2021 Wink. All rights reserved.
+//
+
+import Cocoa
+
+class DocumentViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
